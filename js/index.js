277b1d6697026f97b363fa281b39cd3ab5bc2170
Your Name <gawr1k@DESKTOP-32EG15N>
2023-06-03 02:35:27 +0500

    finish

---
+++ js/index.js
@@ -0,0 +1,48 @@
+let buttonShowAll = document.querySelector('.show-all__button');
+let cardsHidden = document.querySelector('.page__brand__list--hidden');
+
+buttonShowAll.addEventListener('click', function () {
+
+    cardsHidden.classList.toggle('page__brand__list--hidden');
+
+    if (cardsHidden.classList.contains('page__brand__list--hidden')) {
+        buttonShowAll.textContent = 'Показать все';
+
+    } else {
+        buttonShowAll.textContent = 'Скрыть';
+    }
+});
+
+let slider = document.querySelector('.swiper');
+let mySwiper;
+
+function mobileSlider() {
+    if (window.innerWidth <= 767 && slider.dataset.mobile === 'false') {
+        mySwiper = new Swiper(slider, {
+
+            spaceBetween: 16,
+            slidesPerView: 'auto',
+
+
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+        });
+        slider.dataset.mobile = 'true';
+    }
+
+    if (window.innerWidth >= 768) {
+        slider.dataset.mobile = 'false';
+        if (slider.classList.contains('swiper-initialized')) {
+            mySwiper.destroy();
+        }
+
+    }
+}
+
+mobileSlider();
+
+window.addEventListener('resize', () => {
+    mobileSlider();
+})
